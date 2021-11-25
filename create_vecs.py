464704b29d439a0af9c594696333e37de09409ff
lilykaw <lily.kawaoto@gmail.com>
2021-11-25 13:15:39 -0500

    normalized quaternary vecs

--- create_vecs.py
+++ create_vecs.py
@@ -30,52 +30,48 @@ def normalize_vec(li):
 
 ### : Read in english_ipa.csv 
 df = pd.read_csv(CSV_PATH, engine='python')
-df_len = len(df.index)
-df_cols = list(df.columns)
+df_len = len(df.index) # 37 IPA symbols
+df_cols = list(df.columns) # len = 24: 1 IPA + 23 features
 
 
 ### : Create raw vector from csv file
-# @ ipa_vecs: list of ipa feature vectors with values +, -, or 0
-#             37 columns = 1 IPA symbol + 36 features
-ipa_vecs = []
+# @ feature_vecs: list of ipa feature vectors with values +, -, or 0
+#                 24 columns = 24 features
+# @ ipa_list: list of ipa symbols
+ipa_list = []
+feature_vecs = []
 for i in range(df_len):
-    row = df[i:i+1].values.tolist() # a list of 1 list... why?
-    full_vec = row[0]
-    feature_vec = full_vec[1:]
+    full_vec = df[i:i+1].values.tolist()[0] # w/o [0], returns a list of 1 list
     ipa = full_vec[0]
+    feature_vec = full_vec[1:]
     # print("Creating feature vec for /{}/:".format(ipa))
     # print(feature_vec)
-
-    ## note: we may want to keep the IPA symbol for later, so I'll leave it in the vector
-    # ipa_vecs.append(feature_vec)
-    ipa_vecs.append(full_vec) 
+    feature_vecs.append(feature_vec) 
+    ipa_list.append(ipa)
 
 
 ### : Create modified vector using 3 numerical features 
 # @ num_vecs: list of IPA feature vectors with values 1, -1, or 0
 #             the first value in vector is the IPA symbol
 ### : UPDATE 11/25/21 -- use 4 numbers (2, 1, -1, 0). normalize vectors in next step.
-length = len(ipa_vecs[0])
-num_vecs = []
-for ipa in ipa_vecs:
+num_features = len(feature_vecs[0])
+numerical_vecs = []
+for vec in feature_vecs:
     temp = []
-    for i in range(length):
-        if i==0:
-            continue
-        elif ipa[i]=='-':
-            ipa[i] = -1
-        elif ipa[i]=='+':
-            if df_cols[i].isupper():
-                ipa[i] = 2
+    for i in range(num_features):
+        if vec[i]=='-':
+            vec[i] = -1
+        elif vec[i]=='+':
+            if df_cols[i+1].isupper():
+                vec[i] = 2
             else:
-                ipa[i] = 1
+                vec[i] = 1
         else:
-            ipa[i] = 0
-        temp.append(ipa[i])
-    num_vecs.append(temp)
-# print(num_vecs)
-
+            vec[i] = 0
+        temp.append(vec[i])
+    numerical_vecs.append(temp)
+# print(numerical_vecs)
 
 ### : Normalize vector
-norm_vec = [normalize_vec(v) for v in num_vecs]
+norm_vec = [normalize_vec(v) for v in numerical_vecs]
 print(norm_vec)
