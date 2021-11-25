847ea02418badcce0fe2a2d529c3c4e93cbd1a55
lilykaw <lily.kawaoto@gmail.com>
2021-11-25 15:38:13 -0500

    creates weighted graph matrix

---
+++ create_graph_matrix.py
@@ -0,0 +1,21 @@
+'''
+
+L542 Fall 2021
+Final Project
+Lily Kawaoto <lkawaoto@iu.edu>
+
+"Creates a matrix of node-to-node relationship statements.
+ Format of relationship statement: [node1, node2, edge weight]
+ Edge weight is the normalized weight between an IPA symbol and a specific feature."
+
+'''
+
+import os, sys, re
+from numpy.core.numeric import full
+import pandas as pd
+import pprint
+
+
+# change paths if necessary
+VEC_PATH = './norm_vecs.txt'
+
