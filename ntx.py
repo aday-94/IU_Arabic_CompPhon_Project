7cd6d41d7dfe101f9340e4419fac1f8805dc3da9
aday-94 <91922023+aday-94@users.noreply.github.com>
2021-11-30 21:27:48 -0500

    Add files via upload
    #NEWONE

--- ntx.py
+++ ntx.py
@@ -20,10 +20,25 @@ options = {
     'width': 3,
     'arrowstyle': '-|>',
     'arrowsize': 10,
+    'with_labels': True,
 }
 
 G.add_weighted_edges_from(tl)
-nx.draw_networkx(G, arrows=True, **options)
+# use nested for loop like e.g. for i in range of the length of the vector , second one for index position [2]== "0.0", then cont
+#for i in range(len(tl)):
+#    if i == "0.0":
+#        G.remove_edges(i)
+
+nx.draw(G, arrows=True, with_labels=True)
 plt.show()
 
+#edges =  nx.to_numpy_matrix(G, nodelist= G.nodes())
 
+#edges[edges<0] = 0
+
+#G2 = nx.from_numpy_matrix(edges)
+
+#pos=nx.spring_layout(G2)
+#nx.draw_networkx_edges(G2, pos)
+#plt.axis('off')
+#plt.show()
