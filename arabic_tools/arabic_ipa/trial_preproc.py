2ad4ebfada247b703e3ed6a8c72736f2f1f83ead
lilykaw <87586298+lilykaw@users.noreply.github.com>
2022-10-18 17:38:50 -0400

    update trial_preproc.py

--- arabic_tools/arabic_ipa/trial_preproc.py
+++ arabic_tools/arabic_ipa/trial_preproc.py
@@ -22,17 +22,28 @@ arabic_diacritics = re.compile("""
                          """, re.VERBOSE)
 
 arabic_diacritics_dict = {
-    ' ّ':'', # Tashdid: geminate, ie double the previous consonant
+    ' ّ':'', # Tashdid: geminate?, ie double the previous consonant
+    u'\u0651': '', # Shadda: geminate
     ' َ':'a',
     ' ً':'',
     ' ُ':'u',
+    u'\u064F':'u', # Damma
     ' ٌ':'',
-    ' ِ':'i',
+    ' ِ':'i', # Kasra
+    u'\u0650':'i', # Kasra
     ' ٍ':'',
-    ' ْ':'' # explicit absence of vowel
+    ' ْ':'', # explicit absence of vowel
     'ـ':'',
-    '':'ʔ' # Hamza = glottal stop
+    u'\u0654':'ʔ', # Hamza above = glottal stop
+    u'\u0655':'ʔ', # Hamza below = glottal stop
+    u'\u0649':'ʔ', # Alef Maksura? = 
+    u'\u0629':'a', # Teh Marbuta = feminine marker
+    u'\u064D':'in', # Kasratan = postnasalized long vowel /i/
+    u'\u064B':'an', # Fathatan = postnasalized long vowel /a/
+    u'\u064D':'un', # Dammatan = postnasalized long vowel /u/
+    u'\u0652':'' # Sukun = no vowel
 }
+print(list(arabic_diacritics_dict.keys()))
 
 def remove_punctuations(text):
     translator = str.maketrans('', '', punctuations_list)
@@ -42,17 +53,34 @@ def remove_repeating_char(text):
     return re.sub(r'(.)\1+', r'\1', text)
 
 def change_diacritics(text):
+    new_text = ""
     for word in text:
         for i,char in enumerate(word):
-            print((ord(char),char)) # each diacritic gets treated as its own character
-            if char in list(arabic_diacritics_dict.keys()):
-                print(char + " is an arabic diacritic")
-                if char == ' ّ':
-                    char = word[i-1]
-                else:
-                    # print("I am a vowel diacritic\n")
-                    char = arabic_diacritics_dict[char]
-    return text
+            if char == (u'\u0654' or u'\u0655'): # glottal stop
+                new_text += arabic_diacritics_dict[u'\u0654'] 
+            elif char == u'\u0629': # 'a'
+                new_text += arabic_diacritics_dict[u'\u0629'] 
+            elif char == u'\u0649': # glottal stop
+                new_text += arabic_diacritics_dict[u'\u0649']
+            elif char == u'\u0650': # i 
+                new_text += arabic_diacritics_dict[u'\u0650']
+            elif char == u'\u064F': # u
+                new_text += arabic_diacritics_dict[u'\u064F']
+            elif char == u'\u064D': # in
+                new_text += arabic_diacritics_dict[u'\u064D']
+            elif char == u'\u064B': # an
+                new_text += arabic_diacritics_dict[u'\u064B']
+            elif char == u'\u064D': # in
+                new_text += arabic_diacritics_dict[u'\u064D']
+            elif char == u'\u0651': # geminate consonant
+                new_text += word[i-1]
+            elif char == u'\u0652':
+                continue
+            else:
+                new_text += char
+            # print((ord(char),char)) # each diacritic gets treated as its own character    
+    print(new_text)        
+    return new_text
 
 parser = argparse.ArgumentParser(description='Pre-process arabic text (remove '
                                              'diacritics, punctuations, and repeating '
